efb72e36d732452e8cd73a68ad9a32f5a0b1f450
samuelwasini <72897194+samuelwasini@users.noreply.github.com>
2022-11-25 13:36:19 +0700

    Add files via upload
    New  Bukti

--- Bukti.java
+++ Bukti.java
@@ -11,8 +11,8 @@ import java.text.SimpleDateFormat;
 
 public class Bukti
 {
-    // instance variables - replace the example below with your own
-    String pemesan; int mieGor; int mieKuah; int mieIjo; int mieRen;
+    String pemesan; 
+    int mieGor, mieKuah,  mieIjo, mieRen;
     
     public Bukti(String pemesan, int mieGor, int mieKuah, int mieIjo, int mieRen)
     {
@@ -35,6 +35,19 @@ public class Bukti
         {
             System.out.println("Mie Goreng\tx"+mieGor+"\n\t\t\t"+(10000*mieGor));
         } 
+        if(mieKuah>0)
+        {
+            System.out.println("Mie Kuah\tx"+mieKuah+"\n\t\t\t"+(10000*mieKuah));
+        }
+        if(mieIjo>0)
+        {
+            System.out.println("Mie Ijo\tx"+mieIjo+"\n\t\t\t"+(10000*mieIjo));
+        } 
+        if(mieRen>0)
+        {
+            System.out.println("Mie Rendang\tx"+mieRen+"\n\t\t\t"+(10000*mieRen));
+        }
+
         
         int total = (mieGor + mieKuah + mieIjo + mieRen)*10000;
         System.out.println("\n\nTotal Harga :\t\t"+ total);
